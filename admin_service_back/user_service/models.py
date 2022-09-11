98aeb7b7da071c827f5ff0ddc986464d5d2a9891
rostyslav.panto <rost.panto@gmail.com>
2022-09-11 18:29:57 +0300

    Add string representation for user and group models

--- admin_service_back/user_service/models.py
+++ admin_service_back/user_service/models.py
@@ -7,8 +7,13 @@ class Group(models.Model):
     group_name = models.CharField(max_length=120, unique=True)
     description = models.TextField()
 
+    def __str__(self):
+        return self.group_name
 
 class MyUser(models.Model):
     user_name = models.CharField(max_length=120, unique=True)
     created = models.DateField(default=date.today)
     group = models.ForeignKey(Group, default=None, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.user_name
